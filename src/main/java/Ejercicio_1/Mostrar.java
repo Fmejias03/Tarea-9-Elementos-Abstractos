6ca5365969af96d213195801255f0d7453492205
Fer <Fmeji@hotmail.es>
2023-03-19 12:00:25 +0100

    Ejercicio 1 Apartado c) No terminado

---
+++ src/main/java/Ejercicio_1/Mostrar.java
@@ -0,0 +1,41 @@
+package Ejercicio_1;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Mostrar {
+
+    public void menu(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hola, bienvenido a la tienda de alquiler de bicicletas, segways y giroscopios");
+        System.out.println("Este es nuestro catalogo: ");
+        System.out.println("Bicis Lapierre speed 400 (1 año) 27 velocidades                  4,90€/hora");
+        System.out.println("Bicis Btwin riverside 900 (0 años) 10 velocidades                4,90€/hora ");
+        System.out.println("Giroscpio Segway Nine (0 años) 40 km de autonomía [1h50 min]     9,90€/hora");
+        System.out.println("Giroscopio Weebot Echo (1 año) 35 km de autonomía [1h60 min]     9,90€/hora");
+        System.out.println("Segway Ninebot One E+ (0 años) 30 km de autonomía               18,90€/hora");
+
+        int opcion = sc.nextInt();
+
+        System.out.println("¿Que desea alquilar? (1-4)");
+        System.out.println("1. Bicicleta");
+        System.out.println("2. Giroscopio");
+        System.out.println("3. Segway");
+        System.out.println("4. Salir");
+
+        if (opcion == 1){
+            System.out.println("¿Que bicicleta desea alquilar? (1-2)");
+            System.out.println("1. Bicis Lapierre speed 400 (1 año) 27 velocidades");
+            System.out.println("2. Bicis Btwin riverside 900 (0 años) 10 velocidades");
+            int opci = sc.nextInt();
+
+            if ()
+
+        }
+
+
+
+
+    }
+
+}
